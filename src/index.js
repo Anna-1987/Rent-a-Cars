b1fad1f56b8aef4c526747aa3aeb2751813193dc
Anna-1987 <mag260910@gmail.com>
2024-02-04 22:05:58 +0200

    comm

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
 
-        <BrowserRouter basename="/rent-a-cars">
+        <BrowserRouter basename="/Rent-a-Cars">
           <App />
         </BrowserRouter>
                 </PersistGate>
